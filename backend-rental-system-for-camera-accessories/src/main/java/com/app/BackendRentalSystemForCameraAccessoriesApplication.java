213ea5b78765fd9887c13dce776869f17aa0fbfb
MayurSutar1977 <sutarmayur1977@gmail.com>
2022-09-24 11:32:16 +0530

    testing spring starter project

---
+++ backend-rental-system-for-camera-accessories/src/main/java/com/app/BackendRentalSystemForCameraAccessoriesApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRentalSystemForCameraAccessoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRentalSystemForCameraAccessoriesApplication.class, args);
+	}
+
+}
